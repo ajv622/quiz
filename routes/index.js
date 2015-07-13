bda7b485985d7a7cfc244a3a53d34b76f4013090
Adrian E. Jimenez Vega <cansado.de.probar@gmail.com>
2015-07-13 19:34:58 +0200

    Lista preguntas y autoload

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-// Autor
+
 router.get('/author', function(req, res) {
   res.render('author', {});
 });
 
-// autoload
 router.param('quizId', quizController.load);
 
 // Definición de rutas de /quizes
