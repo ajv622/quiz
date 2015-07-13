d1fe06f03bd62f65c53c38110ce39cf61323f404
Adrian E. Jimenez Vega <cansado.de.probar@gmail.com>
2015-07-13 19:23:17 +0200

    Correcciones despliegue BD en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,47 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-	models.Quiz.findAll().success(function(quiz) {
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
-	})
+// Autoload :id
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+  	function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{next(new Error('No existe quizId=' + quizId))}
+    }
+  ).catch(function(error){next(error)});
 };
 
-// GET /quizes/answer
+
+exports.index = function(req, res) {  
+  if (typeof req.query.search !== "undefined" && req.query.search !== "" ) {
+    models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.search + '%']}).then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes, search: req.query.search});
+    }
+    ).catch(function(error) {next(error);})
+  }else{
+    models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes, search: undefined });
+    }
+    ).catch(function(error) {next(error);})
+  }
+};
+
+
+//GET /quizes/:id
+
+exports.show = function(req, res) {
+		res.render('quizes/show', {quiz: req.quiz});
+};
+
+//GET /quizes/answer
+
 exports.answer = function(req, res) {
-	models.Quiz.findAll().success(
-		function(quiz) {
-			if (req.query.respuesta === quiz[0].respuesta) {
-				res.render('quizes/answer', {respuesta: 'Correcto'});
-			} else {
-				res.render('quizes/answer', {respuesta: 'Incorrecto'});
-			}
-		}
-	)
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autor
+router.get('/author', function(req, res) {
+  res.render('author', {});
+});
+
+// autoload
+router.param('quizId', quizController.load);
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
