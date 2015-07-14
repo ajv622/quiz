57199650719a578cd383a9518bf11420b8a8e26c
Adrian E. Jimenez Vega <cansado.de.probar@gmail.com>
2015-07-14 20:04:05 +0200

    Índice temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,7 +25,7 @@ exports.index = function(req, res) {
   }else{
     models.Quiz.findAll().then(
     function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes, search: undefined });
+      res.render('quizes/index.ejs', {quizes: quizes, search: undefined, errors:[] });
     }
     ).catch(function(error) {next(error);})
   }
@@ -35,7 +35,7 @@ exports.index = function(req, res) {
 //GET /quizes/:id
 
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors:[]});
 };
 
 //GET /quizes/answer
@@ -45,5 +45,65 @@ exports.answer = function(req, res) {
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors:[]});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( // crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta", tema: "otro"});
+	res.render('quizes/new', {quiz:quiz, errors:[]});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	var errors = quiz.validate();
+	if (errors) {
+		var i = 0; 
+		var errores = new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+		for (var prop in errors) {
+			errores[i++]={message: errors[prop]};
+		}
+		res.render('quizes/new', {quiz: quiz, errors: errores});
+	} else {
+		quiz // save: guarda en DB campos pregunta y respuesta de quiz
+		.save({fields: ["pregunta", "respuesta", "tema"]})
+		.then( function(){ res.redirect('/quizes')}) ;
+	}
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // por autoload
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
+	
+	var errors = req.quiz.validate();
+	if (errors) {
+		var i = 0; 
+		var errores = new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+		for (var prop in errors) {
+			errores[i++]={message: errors[prop]};
+		}
+		res.render('quizes/edit', {quiz: req.quiz, errors: errores});
+	} else {
+		req.quiz // save: guarda en DB campos pregunta y respuesta de quiz
+		.save({fields: ["pregunta", "respuesta", "tema"]})
+		.then( function(){ res.redirect('/quizes')}) ;
+	}
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then( function() {
+		res.redirect('/quizes');
+	}).catch(function(error) { next(error) });
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,5 +1,8 @@
 // Definición del modelo de Quiz
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('Quiz', { pregunta: DataTypes.STRING
-									, respuesta: DataTypes.STRING});
+	return sequelize.define('Quiz', { pregunta: { type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta Pregunta"}}}
+									, respuesta:{ type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta Respuesta"}}}
+									, tema:{ type: DataTypes.STRING, validate: { notEmpty: {msg: "-> Falta Tema"}, isIn: [['humanidades', 'ocio', 'ciencia', 'tecnologia', 'otro']]}}
+									}
+							);
 }
